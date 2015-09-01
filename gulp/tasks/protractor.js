ee7f3e5f0ac421b5295c94801fa1079071f44bc3
Jake Marsh <jakemmarsh@gmail.com>
2015-08-31 22:25:35 -0700

    update gulp-protractor, fix logic to exit after tests

--- gulp/tasks/protractor.js
+++ gulp/tasks/protractor.js
@@ -9,15 +9,16 @@ var config          = require('../config');
 gulp.task('webdriver-update', webdriverUpdate);
 gulp.task('webdriver', webdriver);
 
-gulp.task('protractor', ['webdriver-update', 'webdriver', 'server'], function() {
+gulp.task('protractor', ['webdriver-update', 'webdriver', 'server'], function(cb) {
 
-  return gulp.src('test/e2e/**/*.js')
-    .pipe(protractor({
-        configFile: config.test.protractor
-    }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
+  gulp.src('test/e2e/**/*.js').pipe(protractor({
+      configFile: config.test.protractor
+  })).on('error', function(err) {
+    // Make sure failed tests cause gulp to exit non-zero
+    throw err;
+  }).on('end', function() {
+    process.exit();
+    cb();
+  });
 
 });
