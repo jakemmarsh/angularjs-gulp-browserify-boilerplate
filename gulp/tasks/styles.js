810934a14138898f82fd986a6500450c0f1f1322
Jake Marsh <jakemmarsh@gmail.com>
2015-08-31 22:29:43 -0700

    update gulp-sass to newest version

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,6 @@
 var config       = require('../config');
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
-var gulpif       = require('gulp-if');
 var handleErrors = require('../util/handleErrors');
 var browserSync  = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
@@ -16,7 +15,7 @@ gulp.task('styles', function () {
       sourceMap: 'sass',
       outputStyle: global.isProd ? 'compressed' : 'nested'
     }))
-    .pipe(autoprefixer("last 2 versions", "> 1%", "ie 8"))
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
     .on('error', handleErrors)
     .pipe(gulp.dest(config.styles.dest))
     .pipe(browserSync.reload({ stream: true }));
