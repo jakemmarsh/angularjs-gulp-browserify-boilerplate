7df272670a2a1a1652cc6dd03834ae54ae2a51ad
Brett Striker <strikeforcezero@gmail.com>
2015-11-08 20:35:19 -0500

    Fix regex for browserSync asset filter to make it more explicit.

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -8,7 +8,7 @@ import gulp        from 'gulp';
 gulp.task('browserSync', function() {
 
   const DEFAULT_FILE = 'index.html';
-  const ASSET_EXTENSION_REGEX = new RegExp(`\\b(?!\\?)\\.(${config.assetExtensions.join('|')})(?!\\.)`, 'i');
+  const ASSET_EXTENSION_REGEX = new RegExp(`\\b(?!\\?)\\.(${config.assetExtensions.join('|')})\\b(?!\\.)`, 'i');
 
   browserSync.init({
     server: {
