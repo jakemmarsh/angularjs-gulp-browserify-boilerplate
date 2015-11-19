110c643c1b9baae72992b358d8c18c74770af576
Jake Marsh <jake@wealthfront.com>
2015-11-18 16:18:50 -0800

    ensure all asset tasks are done before browserify

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -4,11 +4,11 @@ import gulp        from 'gulp';
 import runSequence from 'run-sequence';
 
 gulp.task('prod', ['clean'], function(cb) {
-  
+
   cb = cb || function() {};
 
   global.isProd = true;
 
-  runSequence(['styles', 'images', 'fonts', 'views', 'browserify'], 'gzip', cb);
+  runSequence(['styles', 'images', 'fonts', 'views'], 'browserify', 'gzip', cb);
 
 });
