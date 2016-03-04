1af6e2e85167068f514bfc17d065fcb9df467c17
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2016-03-04 10:19:36 +0200

    rerun tests when template is changed

--- gulp/tasks/tdd.js
+++ gulp/tasks/tdd.js
@@ -7,6 +7,8 @@ import { Server } from 'karma';
 
 gulp.task('tdd', ['views'], function(cb) {
 
+    gulp.watch(config.views.watch, ['views']);
+
     new Server({
         configFile: path.resolve(__dirname, '../..', config.test.karma),
         singleRun: false
