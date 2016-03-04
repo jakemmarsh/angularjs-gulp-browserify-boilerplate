f48546cbf4991d05f9e5ea073117d78ecefc1d4a
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2016-03-04 09:49:12 +0200

    add tdd task

--- app/js/directives/example.js
+++ app/js/directives/example.js
@@ -1,21 +1,21 @@
 function ExampleDirective() {
 
-  return {
-    restrict: 'EA',
-    templateUrl: 'directives/example.html',
-    scope: {
-      title: '@',
-      message: '@clickMessage'
-    },
-    link: (scope, element) => {
-      element.on('click', () => {
-        window.alert('Element clicked: ' + scope.message);
-      });
-    }
-  };
+    return {
+        restrict: 'EA',
+        templateUrl: 'directives/example.html',
+        scope: {
+            title: '@',
+            message: '@clickMessage'
+        },
+        link: (scope, element) => {
+            element.on('click', () => {
+                window.alert('Element clicked: ' + scope.message);
+            });
+        }
+    };
 }
 
 export default {
-  name: 'exampleDirective',
-  fn: ExampleDirective
+    name: 'exampleDirective',
+    fn: ExampleDirective
 };
---
+++ gulp/tasks/tdd.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import config from '../config';
+import path from 'path';
+import gulp from 'gulp';
+import { Server } from 'karma';
+
+gulp.task('tdd', ['views'], function(cb) {
+
+    new Server({
+        configFile: path.resolve(__dirname, '../..', config.test.karma),
+        singleRun: false
+    }, cb).start();
+
+});
